c0267c1e065a77ba58a225e2a934539399b5897e
Sebastian Villa <sebastian@villamedia.ca>
2015-12-16 15:28:30 -0500

    added DB details

--- index.php
+++ index.php
@@ -6,22 +6,33 @@
   </head>
   <body>
     <h1>Hello world</h1>
-    <p>
+    <h2>Environment vars</h2>
+    <code>
+      <?php print_r($_ENV);  ?>
+    </code>
+    <h2>
       The code below will output the results from connecting to MySQL
-    </p>
+    </h2>
     <?php
-      $servername = 'localhost';
-      $username = 'username';
-      $password = 'password';
+      $servername = $_ENV["MYSQL_SERVICE_HOST"];
+      $username = $_ENV["MYSQL_USER"];
+      $password = $_ENV["MYSQL_PASSWORD"];
+      $database = $_ENV["MYSQL_DATABASE"];
 
       try {
-          $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+          $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
           // set the PDO error mode to exception
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           echo 'Connected successfully';
       } catch (PDOException $e) {
           echo 'Connection failed: '.$e->getMessage();
       }
+    ?>
+    <h2>phpinfo</h2>
+    <?php
+
+      phpinfo();
+
     ?>
   </body>
 </html>
